e02400f8b1d91382fae227caed2800f568d6cfee
AbbasRahimi <AbbasRahimi@users.noreply.github.com>
2021-03-18 21:04:10 +0330

    Update discriminator.py
    fix Variable

--- src/GraphGAN/discriminator.py
+++ src/GraphGAN/discriminator.py
@@ -8,7 +8,7 @@ class Discriminator(object):
         self.node_emd_init = node_emd_init
 
         with tf.compat.v1.variable_scope('discriminator'):
-            self.embedding_matrix = tf.get_variable(name="embedding",
+            self.embedding_matrix = tf.Variable(name="embedding",
                                                     shape=self.node_emd_init.shape,
                                                     initializer=tf.constant_initializer(self.node_emd_init),
                                                     trainable=True)
