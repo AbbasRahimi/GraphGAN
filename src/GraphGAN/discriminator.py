a9a48188fb1d27fe55fc9b95223ca74875b63ebc
AbbasRahimi <AbbasRahimi@users.noreply.github.com>
2021-03-20 22:54:35 +0330

    Update discriminator.py

--- src/GraphGAN/discriminator.py
+++ src/GraphGAN/discriminator.py
@@ -8,15 +8,15 @@ class Discriminator(object):
         self.node_emd_init = node_emd_init
 
         with tf.compat.v1.variable_scope('discriminator'):
-            self.embedding_matrix = tf.Variable(name="embedding",
+            self.embedding_matrix = tf.compat.v1.get_variable(name="embedding",
                                                     shape=self.node_emd_init.shape,
-                                                    initializer=tf.constant_initializer(self.node_emd_init),
+                                                    initializer=tf.compat.v1.constant_initializer(self.node_emd_init),
                                                     trainable=True)
             self.bias_vector = tf.Variable(tf.zeros([self.n_node]))
 
-        self.node_id = tf.placeholder(tf.int32, shape=[None])
-        self.node_neighbor_id = tf.placeholder(tf.int32, shape=[None])
-        self.label = tf.placeholder(tf.float32, shape=[None])
+        self.node_id = tf.compat.v1.placeholder(tf.int32, shape=[None])
+        self.node_neighbor_id = tf.compat.v1.placeholder(tf.int32, shape=[None])
+        self.label = tf.compat.v1.placeholder(tf.float32, shape=[None])
 
         self.node_embedding = tf.nn.embedding_lookup(self.embedding_matrix, self.node_id)
         self.node_neighbor_embedding = tf.nn.embedding_lookup(self.embedding_matrix, self.node_neighbor_id)
@@ -28,7 +28,7 @@ class Discriminator(object):
                 tf.nn.l2_loss(self.node_neighbor_embedding) +
                 tf.nn.l2_loss(self.node_embedding) +
                 tf.nn.l2_loss(self.bias))
-        optimizer = tf.train.AdamOptimizer(config.lr_dis)
+        optimizer = tf.optimizers.Adam(config.lr_dis)
         self.d_updates = optimizer.minimize(self.loss)
         self.score = tf.clip_by_value(self.score, clip_value_min=-10, clip_value_max=10)
-        self.reward = tf.log(1 + tf.exp(self.score))
+        self.reward = tf.compat.v1.log(1 + tf.exp(self.score))
